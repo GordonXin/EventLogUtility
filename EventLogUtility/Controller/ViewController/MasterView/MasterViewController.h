706847ee80db4bd6431a60271d4132e43373b677
GordonXin <gordonxin@Gordons-MacBook-Pro.local>
2015-12-30 10:51:50 +0800

    Add BaseView and MasterViewController

---
+++ EventLogUtility/Controller/ViewController/MasterView/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  EventLogUtility
+//
+//  Created by GordonXIn on 12/30/15.
+//  Copyright © 2015 Sapphire. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MasterViewController : NSViewController
+
+@end
---
+++ EventLogUtility/Controller/ViewController/MasterView/MasterViewController.m
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.m
+//  EventLogUtility
+//
+//  Created by GordonXIn on 12/30/15.
+//  Copyright © 2015 Sapphire. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@end
+
+@implementation MasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
