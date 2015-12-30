88fa078122306456f91ba24a61e2fcb66fd934be
GordonXin <gordonxin@Gordons-MacBook-Pro.local>
2015-12-30 10:17:26 +0800

    Add BaseViewController

---
+++ EventLogUtility/Controller/BaseViewController.h
@@ -0,0 +1,70 @@
+//
+//  BaseViewController.h
+//  EventLogUtility
+//
+//  Created by GordonXIn on 12/30/15.
+//  Copyright © 2015 Sapphire. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@class Document;
+
+@interface BaseViewController : NSViewController
+
+#pragma mark init methods
+-(instancetype)init;
+
+-(instancetype)initWithCoder:(NSCoder *)coder;
+
+-(instancetype)initWithNibName:(NSString *)nibNameOrNil
+                        bundle:(NSBundle *)nibBundleOrNil;
+
+#pragma mark over ride methods
+-(void)viewDidLoad;
+
+-(void)viewWillAppear;
+
+-(void)viewDidAppear;
+
+-(void)viewWillLayout;
+
+-(void)viewDidLayout;
+
+-(void)viewWillDisappear;
+
+-(void)viewDidDisappear;
+
+-(void)dealloc;
+
+-(void)awakeFromNib;
+
+-(void)prepareDealloc;
+
+-(void)awakeFromView;
+
+#pragma mark subview controllers methods
+-(void)addSubviewController:(NSViewController *)aController;
+
+-(void)addSubviewController:(NSViewController *)aController
+             withIdentifier:(NSString *)aIdentifier;
+
+@property (nonatomic, readonly, copy) NSArray *subviewControllers;
+
+@property (nonatomic, readonly, copy) NSArray *subViewControllerIdentifers;
+
+-(id)subviewControllerWithIdentifier:(NSString *)aIdentifier;
+
+-(id)subviewControllerAtIndex:(NSUInteger)index;
+
+-(void)removeSubviewController:(NSViewController *)aController;
+
+-(void)removeAllSubviewControllers;
+
+#pragma mark super view controller
+@property (nonatomic, readwrite, weak) BaseViewController *superViewController;
+
+#pragma mark data
+@property (nonatomic, readwrite, weak) Document *document;
+
+@end
---
+++ EventLogUtility/Controller/BaseViewController.m
@@ -0,0 +1,234 @@
+//
+//  BaseViewController.m
+//  EventLogUtility
+//
+//  Created by GordonXIn on 12/30/15.
+//  Copyright © 2015 Sapphire. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+{
+    NSMutableArray      *_arySubviewControllers;
+    BOOL                 _isAppeared;
+    BOOL                 _isDealloced;
+}
+
+@end
+
+@implementation BaseViewController
+
+@dynamic subviewControllers;
+@dynamic subViewControllerIdentifers;
+
+#pragma mark init methods
+
+-(instancetype)init
+{
+    if (self = [super init])
+    {
+        [self baseInitInner];
+    }
+    return self;
+}
+
+-(instancetype)initWithCoder:(NSCoder *)coder
+{
+    if (self = [super initWithCoder:coder])
+    {
+        [self baseInitInner];
+    }
+    return self;
+}
+
+-(instancetype)initWithNibName:(NSString *)nibNameOrNil
+                        bundle:(NSBundle *)nibBundleOrNil
+{
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])
+    {
+        [self baseInitInner];
+    }
+    return self;
+}
+
+-(void)baseInitInner
+{
+    _arySubviewControllers = [NSMutableArray array];
+    _isAppeared = NO;
+    _isDealloced = NO;
+}
+
+#pragma mark over ride methods
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+-(void)viewWillAppear
+{
+    [super viewWillDisappear];
+    
+    if (!_isAppeared)
+    {
+        _isAppeared = YES;
+        [self awakeFromView];
+    }
+}
+
+-(void)viewDidAppear
+{
+    [super viewDidAppear];
+}
+
+-(void)viewWillLayout
+{
+    [super viewWillLayout];
+}
+
+-(void)viewDidLayout
+{
+    [super viewDidLayout];
+}
+
+-(void)viewWillDisappear
+{
+    [super viewWillDisappear];
+}
+
+-(void)viewDidDisappear
+{
+    [super viewDidDisappear];
+}
+
+-(void)dealloc
+{
+    if (!_isDealloced)
+    {
+        [self prepareDealloc];
+        _isDealloced = NO;
+    }
+}
+
+-(void)awakeFromNib
+{
+    [super awakeFromNib];
+}
+
+-(void)prepareDealloc
+{
+    
+}
+
+-(void)awakeFromView
+{
+    
+}
+
+-(NSString *)identifier
+{
+    if ([[super identifier] length])
+    {
+        return [super identifier];
+    }
+    
+    return NSStringFromClass([self class]);
+}
+
+#pragma mark subview controllers methods
+-(void)addSubviewController:(NSViewController *)aController
+{
+    if (!aController)
+    {
+        return;
+    }
+    if (![[aController identifier] length])
+    {
+        return;
+    }
+    if ([aController respondsToSelector:@selector(setSuperViewController:)])
+    {
+        [aController performSelector:@selector(setSuperViewController:) withObject:self];
+    }
+    [_arySubviewControllers addObject:aController];
+}
+
+-(void)addSubviewController:(NSViewController *)aController
+             withIdentifier:(NSString *)aIdentifier
+{
+    if (!aController)
+    {
+        return;
+    }
+    if (![aIdentifier length])
+    {
+        return;
+    }
+    if ([aController respondsToSelector:@selector(setSuperViewController:)])
+    {
+        [aController performSelector:@selector(setSuperViewController:) withObject:self];
+    }
+    [aController setIdentifier:aIdentifier];
+    [_arySubviewControllers addObject:aController];
+}
+
+-(NSArray *)subviewControllers
+{
+    return [NSArray arrayWithArray:_arySubviewControllers];
+}
+
+-(NSArray *)subviewControllerIdentifiers
+{
+    NSMutableArray *ary = [NSMutableArray arrayWithCapacity:[_arySubviewControllers count]];
+    for (NSViewController *aController in _arySubviewControllers)
+    {
+        [ary addObject:[aController identifier]];
+    }
+    return [NSArray arrayWithArray:ary];
+}
+
+-(id)subviewControllerWithIdentifier:(NSString *)aIdentifier
+{
+    if ([aIdentifier length])
+    {
+        for (NSViewController *aController in _arySubviewControllers)
+        {
+            if ([[aController identifier] isEqualToString:aIdentifier])
+            {
+                return aController;
+            }
+        }
+    }
+    return nil;
+}
+
+-(id)subviewControllerAtIndex:(NSUInteger)index
+{
+    if (index < [_arySubviewControllers count])
+    {
+        return [_arySubviewControllers objectAtIndex:index];
+    }
+    return nil;
+}
+
+-(void)removeSubviewController:(NSViewController *)aController
+{
+    if (!aController)
+    {
+        return;
+    }
+    if (![_arySubviewControllers containsObject:aController])
+    {
+        return;
+    }
+    [_arySubviewControllers removeObject:aController];
+}
+
+-(void)removeAllSubviewControllers
+{
+    [_arySubviewControllers removeAllObjects];
+}
+
+#pragma mark data
+
+@end
